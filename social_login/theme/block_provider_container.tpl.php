c705345363e43946de6ae4d6c81deedafe279f0f
frdpnl <frd.pinel@gmail.com>
2015-06-17 17:32:56 +0200

    initial commit

---
+++ social_login/theme/block_provider_container.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Container in which the social network icons are displayed.
+ */
+
+?>
+<div class="social_login" style="margin:20px 0 10px 0">
+ <div id="<?php echo filter_xss($containerid); ?>"></div>
+</div>
