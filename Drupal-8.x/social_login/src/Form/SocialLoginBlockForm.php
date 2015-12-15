ca1824b94a697cebf9b386e0c62b8829e88560a3
frdpnl <fpinel@oneall.com>
2015-12-15 11:12:08 +0100

    Added block feature. Kept only one twig template.

---
+++ Drupal-8.x/social_login/src/Form/SocialLoginBlockForm.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\social_login\Form\SocialLoginBlockForm.
+ */
+
+namespace Drupal\social_login\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Url;
+
+/**
+ * Builds the Social Login form for the Social Login block.
+ */
+class SocialLoginBlockForm extends FormBase {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getFormId() {
+		return 'social_login_block_form';
+	}
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getEditableConfigNames() {
+		return array();
+	}
+	/**
+	 * {@inheritdoc}
+	 */
+	public function submitForm(array &$form, FormStateInterface $form_state) {
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm(array $form, FormStateInterface $form_state) {
+		global $is_https;
+
+		\Drupal::logger('social_login')->notice('> '. __FUNCTION__ .'@'. __LINE__ .'()');
+
+		// Read Settings.
+		$settings = \social_login_get_settings();
+
+		$title = $settings['login_page_caption'];
+
+		$containerid = 'social_login_providers_' . rand(99999, 9999999);
+		
+		\social_login_add_js_plugin($form, $settings['api_subdomain']);
+
+		$current_uri = \social_login_get_current_url($is_https);
+		$callback_uri = Url::fromRoute('social_login.core', [], array('absolute' => TRUE, 'query' => array('origin' => $current_uri)))->toString();
+		\Drupal::logger('social_login')->notice('- '. __FUNCTION__ .'@'. __LINE__ .' callback:'. $callback_uri);
+
+		$provider_string = "\"". implode("\",\"", $settings['enabled_providers']) ."\"";
+
+		$form['social_login_' . $containerid] = array(
+				'#label' => $title,
+				'#weight' => 0,
+				// render the TWIG template, with it's variables and JS:
+				'#theme' => 'provider_container',
+				'#containerid' => $containerid,
+				'#plugintype' => 'social_login',
+				'#providers' => $provider_string,
+				'#token' => '',
+				'#callbackuri' => $callback_uri,
+				// Caching: the cache tag is the callback uri (so we can redirect to the same page), so the url context is suitable:
+				'#cache' => array(
+					'contexts' => array('url'),
+				),
+		);
+		$renderer = \Drupal::service('renderer');
+		$renderer->addCacheableDependency($form, $callback_uri);
+		return $form;
+	}
+
+}
+?>
