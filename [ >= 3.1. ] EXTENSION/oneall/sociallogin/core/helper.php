08a7e7cc270f590bb03d61ef4b9b6af7b94f8557
cschlesser <cschlesser@oneall.com>
2017-06-07 09:50:57 +0200

    Version increase

--- [ >= 3.1. ] EXTENSION/oneall/sociallogin/core/helper.php
+++ [ >= 3.1. ] EXTENSION/oneall/sociallogin/core/helper.php
@@ -30,7 +30,7 @@ namespace oneall\sociallogin\core;
 class helper
 {
     // Version
-    const USER_AGENT = 'SocialLogin/3.1 phpBB/3.1.x (+http://www.oneall.com/)';
+    const USER_AGENT = 'SocialLogin/3.2 phpBB/3.1.x (+http://www.oneall.com/)';
 
     // @var \phpbb\config\config
     protected $config;
