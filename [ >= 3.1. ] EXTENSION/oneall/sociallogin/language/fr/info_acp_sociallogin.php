abba0a849cf8be7a16eb11de2842ec6cc2759493
Raphaël M <Galixte@users.noreply.github.com>
2017-07-09 00:01:39 +0200

    Update of the FRENC translation for the OneAll Social Login extension

--- [ >= 3.1. ] EXTENSION/oneall/sociallogin/language/fr/info_acp_sociallogin.php
+++ [ >= 3.1. ] EXTENSION/oneall/sociallogin/language/fr/info_acp_sociallogin.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * @package   	OneAll Social Login
+ *
+ * OneAll Social Login. An extension for the phpBB Forum Software package.
+ * French translation by Galixte (http://www.galixte.com)
+ *
  * @copyright 	Copyright 2011-2017 http://www.oneall.com
  * @license   	GNU/GPL 2 or later
  *
@@ -22,6 +25,10 @@
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
  */
+
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -32,7 +39,23 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-    'OA_SOCIAL_LOGIN_ACP_AUTH_SETTTING_INFO' => 'Offre authentification avec nom d’utilisateur/mot de passe ainsi que la connexion avec le compte d’un résau social. Les réseaux sociaux sont configurables sous EXTENSIONS \ ONEALL SOCIAL LOGIN.',
+    'OA_SOCIAL_LOGIN_ACP_AUTH_SETTTING_INFO' => 'Permet l’authentification au moyen d’un nom d’utilisateur et d’un mot de passe d’un compte utilisateur du forum ou au moyen des identifiants d’un compte d’un réseau social. Les paramètres de l’extension sont disponibles depuis l’onglet « EXTENSIONS », page « Passerelle de connexion OneAll ».',
 ));
 
