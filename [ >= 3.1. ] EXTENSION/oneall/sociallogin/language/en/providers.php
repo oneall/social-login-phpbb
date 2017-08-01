0102653b9363ea5c8a74c9b3c7a45393d7178509
cschlesser <cschlesser@oneall.com>
2017-08-01 11:16:12 +0200

    Licence tag fixed

--- [ >= 3.1. ] EXTENSION/oneall/sociallogin/language/en/providers.php
+++ [ >= 3.1. ] EXTENSION/oneall/sociallogin/language/en/providers.php
@@ -2,7 +2,7 @@
 /**
  * @package   	OneAll Social Login
  * @copyright 	Copyright 2011-2017 http://www.oneall.com
- * @license   	GNU/GPL 2
+ * @license   	GPL-2.0
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
