d0e876bc93cf87d8811ec41ab10adfe083cc1608
Damien <zara.damien21@gmail.com>
2018-01-18 09:56:12 +0100

    Block crash fixed

--- upload/social_login/src/Form/SocialLoginBlockForm.php
+++ upload/social_login/src/Form/SocialLoginBlockForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\social_login\Form;
 
+use Drupal;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
@@ -10,74 +11,78 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Builds the form for the Social Login block.
  */
-class SocialLoginBlockForm extends FormBase {
+class SocialLoginBlockForm extends FormBase
+{
+    /**
+     * Determines the ID of the form.
+     */
+    public function getFormId()
+    {
+        return 'social_login_block_form';
+    }
 
-  /**
-   * Determines the ID of the form.
-   */
-  public function getFormId() {
-    return 'social_login_block_form';
-  }
+    /**
+     * Gets the configuration names that will be editable.
+     */
+    public function getEditableConfigNames()
+    {
+        return array();
+    }
 
-  /**
-   * Gets the configuration names that will be editable.
-   */
-  public function getEditableConfigNames() {
-    return array();
-  }
+    /**
+     * Form submission handler.
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+    }
 
-  /**
-   * Form submission handler.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-  }
+    /**
+     * Form constructor.
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        // Are we using HTTPs?
+        $is_https = Drupal::request()->isSecure();
 
-  /**
-   * Form constructor.
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+        // Read Settings.
+        $settings = \social_login_get_settings();
 
-  	// Are we using HTTPs?
-  	$is_https = Drupal::request()->isSecure();
+        $title = $settings['login_page_caption'];
 
-    // Read Settings.
-    $settings = \social_login_get_settings();
+        $containerid = 'social_login_providers_' . rand(99999, 9999999);
 
-    $title = $settings['login_page_caption'];
+        social_login_add_js_plugin($form, $settings['api_subdomain']);
 
-    $containerid = 'social_login_providers_' . rand(99999, 9999999);
+        $current_uri = \social_login_get_current_url($is_https);
+        $callback_uri = Url::fromRoute('social_login.core', [], array(
+            'absolute' => true,
+            'query' => array(
+                'origin' => $current_uri
+            )
+        ))->toString();
 
-    social_login_add_js_plugin($form, $settings['api_subdomain']);
+        $provider_string = "\"" . implode("\",\"", $settings['enabled_providers']) . "\"";
 
-    $current_uri = \social_login_get_current_url($is_https);
-    $callback_uri = Url::fromRoute('social_login.core', [], array(
-      'absolute' => TRUE,
-      'query' => array(
-        'origin' => $current_uri,
-      ),
-    ))->toString();
+        $form['social_login_' . $containerid] = array(
+            '#label' => $title,
+            '#weight' => 0,
+            '#theme' => 'provider_container',
+            '#containerid' => $containerid,
+            '#plugintype' => 'social_login',
+            '#providers' => $provider_string,
+            '#token' => '',
+            '#callbackuri' => $callback_uri,
+            // The cache tag is the callback uri (redirect to the same page).
+            '#cache' => array(
+                'contexts' => array(
+                    'url'
+                )
+            )
+        );
 
-    $provider_string = "\"" . implode("\",\"", $settings['enabled_providers']) . "\"";
+        $renderer = \Drupal::service('renderer');
+        $renderer->addCacheableDependency($form, $callback_uri);
 
-    $form['social_login_' . $containerid] = array(
-      '#label' => $title,
-      '#weight' => 0,
-      '#theme' => 'provider_container',
-      '#containerid' => $containerid,
-      '#plugintype' => 'social_login',
-      '#providers' => $provider_string,
-      '#token' => '',
-      '#callbackuri' => $callback_uri,
-      // The cache tag is the callback uri (redirect to the same page).
-      '#cache' => array(
-        'contexts' => array(
-          'url',
-        ),
-      ),
-    );
-
-    $renderer = \Drupal::service('renderer');
-    $renderer->addCacheableDependency($form, $callback_uri);
-    return $form;
-  }
+        return $form;
+    }
 }
