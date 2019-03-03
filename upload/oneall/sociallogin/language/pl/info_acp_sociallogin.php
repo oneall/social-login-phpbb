0d3c1980fd26c714f3921250316d3883dc7672e7
rozwad <rozwad@onet.pl>
2019-03-03 20:34:52 +0100

    Polish translation

---
+++ upload/oneall/sociallogin/language/pl/info_acp_sociallogin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package   	OneAll Social Login
+ * @copyright 	Copyright 2011-2017 http://www.oneall.com
+ * @license   	GPL-2.0
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ *
+ * The "GNU General Public License" (GPL) is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ */
+
+/**
+ * Polish translations by Rozwad
+ * https://gwc.pl/forum
+*/
+if (!defined ('IN_PHPBB'))
+{
+	exit ();
+}
+
+if (empty ($lang) || !is_array ($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge ($lang, array(
+	'OA_SOCIAL_LOGIN_ACP_AUTH_SETTTING_INFO' => 'Dostarcza uwierzytelnienia przez nazwę użytkownika/hasło (uwierzytelnienie przez bazę danych, ang. Db Authentication) oraz login przez konto społecznościowe. Uwierzytelnienie przez konto społecznościowe może być skonfigurowane w zakładce ROZSZERZENIA \ ONEALL SOCIAL LOGIN.'
+));
